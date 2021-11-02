488c856088fb27ac4a251cd3f64531f0ad52228d
Rahul Saha <forrahul7@gmail.com>
2021-11-03 00:50:01 +0530

    added dashboard-data to see data for dashboard

--- API/index.php
+++ API/index.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . '/include/init.php';
 session_start();
-//error_reporting(true);
+error_reporting(true);
+error_reporting(-1);
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
@@ -49,6 +50,11 @@ class index
                 require_once __DIR__ . '/templates/forgot.php';
                 break;
 
+            case 'dashboard-data':
+
+                    require_once __DIR__.'/templates/dashboard.php';
+                    break;
+                    
             default:
                 $result = array(
                     'Status' => http_response_code(),
