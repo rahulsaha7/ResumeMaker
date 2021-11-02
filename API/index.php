1f601e422d7e37949076bb4e58df369971d8663d
Rahul Saha <forrahul7@gmail.com>
2021-11-02 20:21:31 +0530

    forgot password logic is added

--- API/index.php
+++ API/index.php
@@ -1,7 +1,8 @@
 <?php
+
 require_once __DIR__ . '/include/init.php';
 session_start();
-//error_reporting(0);
+//error_reporting(true);
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
@@ -36,11 +37,18 @@ class index
                 break;
 
             case 'auth-cred':
+
                 require_once __DIR__ . '/templates/authCred.php';
                 break;
             case "Logout":
+
                 require_once __DIR__ . '/templates/Logout.php';
                 break;
+            case 'forgot-password':
+
+                require_once __DIR__ . '/templates/forgot.php';
+                break;
+
             default:
                 $result = array(
                     'Status' => http_response_code(),
