eda2e6af8b4b6e7f3a9b5c44a840807949a60295
Rahul Saha <forrahul7@gmail.com>
2021-11-20 21:18:06 +0530

    db updated

--- API/include/db.php
+++ API/include/db.php
@@ -2,70 +2,67 @@
 
 
 
-class database{
+class database
+{
     private $conn;
     public $sql_query;
     public $result;
-    function __construct(){
-       
+    function __construct()
+    {
+
         $this->db_connect();
     }
-    function db_connect(){
-        try{
+    function db_connect()
+    {
+        try {
 
-            
-             //$this->conn = new PDO("mysql:host=remotemysql.com;dbname=0ERaAydX2j",'0ERaAydX2j','CkPmhRmMuU');
-             $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."",DB_USER,DB_PASS);
-           
-            
+
+
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . "", DB_USER, DB_PASS);
+
+
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        }catch(PDOException $e){
-            echo 'Something went wrong, please connect with the adminsitrator'.$e;
-            // file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+        } catch (PDOException $e) {
+            echo 'Something went wrong, please connect with the adminsitrator';
         }
     }
 
-    function query($sql){
-        try{
-        $this->sql_query = $this->conn->prepare($sql);
-        $this->sql_query->execute();
-        }catch(PDOException $e){
+    function query($sql)
+    {
+        try {
+            $this->sql_query = $this->conn->prepare($sql);
+            $this->sql_query->execute();
+        } catch (PDOException $e) {
             echo "oops ! looks like something went wrong";
         }
     }
 
-    function query_value($sql,$values){
+    function query_value($sql, $values)
+    {
 
-     
-      try{      
-        $this->sql_query = $this->conn->prepare($sql);
 
-            for ($i=0; $i <count($values) ; $i++) { 
-                $this->sql_query->bindParam($i+1,$values[$i],PDO::PARAM_STR);
+        try {
+            $this->sql_query = $this->conn->prepare($sql);
 
+            for ($i = 0; $i < count($values); $i++) {
+                $this->sql_query->bindParam($i + 1, $values[$i], PDO::PARAM_STR);
             }
-            
-        
-        
-        if($this->sql_query->execute()){
-            $this->result = true;
+
+
+
+            if ($this->sql_query->execute()) {
+                $this->result = true;
+            } else {
+                $this->result = false;
+            }
+        } catch (PDOException $e) {
+            echo "oops ! looks like something went wrong";
         }
-        else{
-            $this->result = false;
-        }
-    }catch(PDOException $e){
-        echo "oops ! looks like something went wrong";
     }
 
+
+    function close_connection()
+    {
+        $this->conn = null;
     }
-
-
-   function close_connection(){
-       $this->conn = null;
-   }
-   
 };
-        
-
-?>
