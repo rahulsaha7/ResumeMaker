119bff89a92c28e58f6d91a8b56d11ec02d833d8
Rahul Saha <forrahul7@gmail.com>
2024-03-03 23:12:48 +0530

    removed password

--- API/include/config.php
+++ API/include/config.php
@@ -11,7 +11,7 @@ if ($env2 == 'development') {
     defined('BASE_URL') ? NULL : define('BASE_URL', 'http://localhost');
     defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
     defined('DB_USER') ? NULL : define('DB_USER', 'root');
-    defined('DB_PASS') ? NULL : define('DB_PASS', 'Rahul@7242');
+    defined('DB_PASS') ? NULL : define('DB_PASS', '');
     defined('DB_NAME') ? NULL : define('DB_NAME', 'MAD');
 } else {
     defined('BASE_URL') ? NULL : define('BASE_URL', 'https://rahulsaha.live');
